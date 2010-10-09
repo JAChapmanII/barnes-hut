d685f410742fcb0d1606329e908685a64644bad2
Jeff Chapman II <JAChapmanII@GMail.com>
2010-10-09 12:17:15 -0400

    Use new functionality

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,7 @@ using std::cin;
 using std::string;
 
 #include "particle_system.hpp"
+#include "quadtree.hpp"
 
 int main( int argc, char** argv )
 {
@@ -48,7 +49,46 @@ int main( int argc, char** argv )
 	cout << "Selected file: " << fileName << "\n\n";
 
 	ParticleSystem mPS( fileName );
+
+	if( mPS.getSize() < 1 )
+	{
+		cout << "No particles in file\n";
+		return 0;
+	}
 	cout << mPS << '\n';
+
+	Particle* cur;
+	cur = mPS.getParticle( 0 );
+	float minX = cur->x,
+			maxX = cur->x,
+			minY = cur->y,
+			maxY = cur->y;
+	for( unsigned int i = 1; i < mPS.getSize(); i++ )
+	{
+		cur = mPS.getParticle( i );
+		if( cur->x < minX )
+			minX = cur->x;
+		if( cur->x > maxX )
+			maxX = cur->x;
+		if( cur->y < minY )
+			minY = cur->y;
+		if( cur->y > maxY )
+			maxY = cur->y;
+	}
+	cout << "min/max X: " << minX << ", " << maxX << "\n";
+	cout << "min/max Y: " << minY << ", " << maxY << "\n";
+
+	cout << "Putting all particles into Quadtree, let's see if we SIGSEGV\n";
+	Quadtree mQT( minX - 1.0, maxX + 1.0, maxY + 1.0, minY - 1.0, NULL );
+	cout << "[" << mQT.getLeft() << ", " << mQT.getRight() << "] ["
+		<< mQT.getBottom() << ", " << mQT.getTop() << "]\n";
+
+	for( unsigned int i = 0; i < mPS.getSize(); i++ )
+	{
+		mQT.add( mPS.getParticle( i ) );
+	}
+
+	cout << "Yay! We didn't crash\n";
 
 	return 0;
 }
