34c2d51f667d8518e38f8d9e4779df614188a8ce
Jeff Chapman II <JAChapmanII@GMail.com>
2010-10-09 10:46:19 -0400

    Comment Quadtree class

--- src/quadtree.hpp
+++ src/quadtree.hpp
@@ -20,9 +20,15 @@
 #ifndef QUADTREE_HPP
 #define QUADTREE_HPP
 
+/**
+ * Class representing a recursive space division into four quadrants.
+ */
 class Quadtree
 {
 	public:
+		/**
+		 * Class used to represent a point with an x, y and mass.
+		 */
 		class Node
 		{ //{{{
 			public:
@@ -42,18 +48,50 @@ class Quadtree
 				}
 		}; //}}}
 
+		/**
+		 * Create a quadtree representing a certain amount of space.
+		 * @param iL : left hand coordinate
+		 * @param iR : right hand coordinate
+		 * @param iT : top coordinate
+		 * @param iB : bottom coordinate
+		 * @param iMe : initial point to contain
+		 * @note : iMe should only be NULL for empty nodes
+		 */
 		Quadtree(float iL, float iR, float iT, float iB, Node* iMe);
 
+		/**
+		 * Add a node to this tree.
+		 * @param node : node to be added
+		 */
 		void add(Node* node);
+		/**
+		 * Add an entire Quadtree to this one.
+		 * @param tree : tree to be added
+		 */
 		void add(Quadtree* tree);
 
+		/**
+		 * Delete all contents of this.
+		 */
 		void clear();
 
+		/**
+		 * Return the point this Quadtree represents.
+		 * @return : point representing average of all nodes in this
+		 */
 		Node* getMe();
 
+		/**
+		 * Return the quadrant a node shoud be point into in this tree.
+		 * @param node : node to find quadrant of
+		 * @return : quadrant where node should go
+		 */
 		unsigned int getQuadrant( Node* node ) const;
 
 	private:
+		/**
+		 * Allocates space for and creates children Quadtrees
+		 */
 		void makeChildren();
 
 		float left, right;
